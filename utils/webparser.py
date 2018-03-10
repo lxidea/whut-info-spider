3bffd49664bc945ec9947b499d4336a2e112f2df
Xin Lai <laixin456@163.com>
2018-03-10 23:20:24 +0800

    extract text from content instead of htmltext
    extract text from content instead of htmltext
    get rid of chinese messy code

--- utils/webparser.py
+++ utils/webparser.py
@@ -11,6 +11,7 @@ class webparser(object):
         super(webparser, self).__init__()
         response = fetcher.fetch(_url)
         self.htmltext = response.text
+        self.content = response.content
         self.status = response.status_code
         self.code_reason = response.reason
         self.time_cost = response.elapsed
@@ -23,11 +24,15 @@ class webparser(object):
         self.history = response.history
         #self.stew()
     def stew(self):
-        self.soup = BeautifulSoup(self.htmltext, 'html.parser')
+        self.soup = BeautifulSoup(self.content, 'html.parser')
         return self.soup
     def text(self):
         return self.htmltext
+    def content(self):
+         return self.content
 
 if __name__ == '__main__':
     #page = fetcher.fetch("http://www.baidu.com")
-    myparser = webparser("http://www.baidu.com")
+    myparser = webparser("http://i.whut.edu.cn")
+    soup = myparser.stew()
+    grablist = [[addr.string, addr.get("href")] for addr in soup.find("div","in_nav").find_all("a") if "i.whut.edu.cn/" in addr.get("href")]
