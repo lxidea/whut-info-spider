ad308994a6eecd6cafea54012c5861af2eb7d6a6
Xin Lai <laixin456@163.com>
2018-08-23 10:10:49 +0800

    add news page reader and news list reader
    we still need to implement the link extractor which can navigate through news list pages

--- utils/webparser.py
+++ utils/webparser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 from fetcher import fetcher
 import requests,datetime
@@ -7,15 +7,20 @@ from bs4 import BeautifulSoup
 
 class webparser(object):
     """docstring for webparser"""
-    def __init__(self, _url):
+    def __init__(self, _url=None, proxy=None):
         super(webparser, self).__init__()
-        response = fetcher.fetch(_url)
+        self.url = _url
+        self.proxy = proxy
+        if _url is None:
+            return
+        response = fetcher.fetch(self.addSchema(_url),self.proxy)
         self.htmltext = response.text
         self.content = response.content
         self.status = response.status_code
         self.code_reason = response.reason
         self.time_cost = response.elapsed
         self.url = response.url
+        #print self.url
         self.ok = response.ok
         self.encoding = response.encoding
         self.cookie = response.cookies
@@ -23,9 +28,22 @@ class webparser(object):
         self.is_redirect = response.is_redirect
         self.history = response.history
         #self.stew()
+    def setProxy(self, _proxy):
+        self.proxy = _proxy
+    def addSchema(self, _url):
+        if not _url.startswith('http'):
+            return 'http://'+_url
+        else:
+            return _url
+    def cleanMe(self, soup):
+        [x.extract() for x in soup.find_all('script')]
+        [x.extract() for x in soup.find_all('style')]
+        [x.extract() for x in soup.find_all('meta')]
+        [x.extract() for x in soup.find_all('noscript')]
+        return soup
     def stew(self, _url):
         if _url!=None:
-            response = fetcher.fetch(_url)
+            response = fetcher.fetch(self.addSchema(_url),self.proxy)
             self.htmltext = response.text
             self.content = response.content
             self.status = response.status_code
@@ -39,11 +57,11 @@ class webparser(object):
             self.is_redirect = response.is_redirect
             self.history = response.history
         self.soup = BeautifulSoup(self.content, 'html.parser')
-        return self.soup
+        return self.cleanMe(self.soup)
     def text(self):
         return self.htmltext
     def content(self):
-         return self.content
+        return self.content
 
 if __name__ == '__main__':
     #page = fetcher.fetch("http://www.baidu.com")
