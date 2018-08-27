ddfad86901641d882ed99fc165d411b437b1496d
Xin Lai <laixin456@163.com>
2018-08-27 23:38:33 +0800

    add createTab method

--- database/sqlconn.py
+++ database/sqlconn.py
@@ -22,7 +22,70 @@ class sqlconn(object):
         if self.connection.open:
             self.connection.close()
 
+    def createTab(self, table, temp=True, ifnotexists=False, fieldlist=[], typelist=[], attribs=[],
+     primKeys=[], forgkeys=[], reftabs=[], refkeys=[], charset='utf8mb4'):
+        lens = [len(fieldlist),len(typelist),len(attribs)]
+        if max(lens)!=min(lens):
+            return False
+        if len(primKeys)==0:
+            return False
+        lens = [len(forgkeys),len(reftabs),len(refkeys)]
+        if max(lens)!=min(lens):
+            return False
+        if temp:
+            tempstr = "temporary"
+        else:
+            tempstr = ""
+        if ifnotexists:
+            ifnostr = "if not exists "
+        else:
+            ifnostr = ""
+        sql = "create " + tempstr + " table " + ifnostr + table +' ('
+        cdefs = ','.join([('`'+field+'` '+typelist[idx]+' '+attribs[idx]).strip() for idx,field in enumerate(fieldlist)])
+        if len(primKeys)>0:
+            primstr = 'primary key (' + ','.join(['`'+x+'`' for x in primKeys]) + ')'
+        else:
+            primstr = ''
+        if len(forgkeys)>0:
+            forkstrs = ','.join(['foreign key (%s) references %s(%s)'%(key,reftabs[idx],refkeys[idx]) for idx,key in enumerate(forgkeys)])
+        else:
+            forkstrs = ''
+        tabopts = ') Engine=InnoDB default charset=' + charset + ' collate=' + charset + '_bin'
+        sql += cdefs + ',' + primstr + forkstrs + tabopts
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql)
+
+        self.connection.commit()
+        return True
+        #except:
+        #return False
+
+    def insert(self, table, keylist, valuelist):
+        if table is None or len(table)==0:
+            return False
+        if type(keylist) is not list or type(valuelist) is not list:
+            return False
+        if len(keylist)*len(valuelist)==0:
+            return False
+        keystr = ', '.join(['`'+x+'`' for x in keylist])
+        values = tuple(valuelist)
+        formats = ', '.join(['%s']*len(values))
+        print keystr
+        print values
+        sql = "INSERT INTO ``" + table + "`` (" + keystr + ") VALUES (" + formats + ")"
+        print sql
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql, values)
+
+        self.connection.commit()
+
 if __name__ == '__main__':
     conn = sqlconn('localhost',3307,'root','usbw')
     if conn.connection.open:
         print 'opened'
+        print conn.createTab('article',False,True,
+        ['id','title','link','content','keyword_id','category_id','datetime','source_id','attachment_id'],
+         ['bigint','varchar(100)','varchar(120)','text','int','int','date','int','int'],
+          ['auto_increment','not null','not null','not null','not null','not null','not null','not null','not null'],
+           ['id'], [], [], [], 'utf8mb4')
+        #conn.insert('tab',['name','key','passwd'],['John',2,'123456'])
