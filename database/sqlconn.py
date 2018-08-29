a91c2989b63403d60fb88a13620c4ce0f361cc4c
Xin Lai <laixin456@163.com>
2018-08-29 16:16:01 +0800

    minor change

--- database/sqlconn.py
+++ database/sqlconn.py
@@ -24,8 +24,8 @@ class sqlconn(object):
         if self.connection.open:
             self.connection.close()
 
-    def createTab(self, table, temp=True, ifnotexists=False, fieldlist=[], typelist=[], attribs=[],
-     primKeys=[], forgkeys=[], reftabs=[], refkeys=[], charset='utf8mb4'):
+    def createTab(self, table, fieldlist=[], typelist=[], attribs=[],
+     primKeys=[], forgkeys=[], reftabs=[], refkeys=[], charset='utf8mb4',temp=False, ifnotexists=True):
         lens = [len(fieldlist),len(typelist),len(attribs)]
         if max(lens)!=min(lens):
             return False, 'parameters not match'
@@ -35,25 +35,25 @@ class sqlconn(object):
         if max(lens)!=min(lens):
             return False, 'foreign keys not match'
         if temp:
-            tempstr = "temporary"
+            tempstr = "temporary "
         else:
             tempstr = ""
         if ifnotexists:
             ifnostr = "if not exists "
         else:
             ifnostr = ""
-        sql = "create " + tempstr + " table " + ifnostr + table +' ('
+        sql = "create " + tempstr + "table " + ifnostr + table +' ('
         cdefs = ','.join([('`'+field+'` '+typelist[idx]+' '+attribs[idx]).strip() for idx,field in enumerate(fieldlist)])
         if len(primKeys)>0:
-            primstr = 'primary key (' + ','.join(['`'+x+'`' for x in primKeys]) + ')'
+            primstr = ', primary key (' + ','.join(['`'+x+'`' for x in primKeys]) + ')'
         else:
             primstr = ''
         if len(forgkeys)>0:
-            forkstrs = ','.join(['foreign key (%s) references %s(%s)'%(key,reftabs[idx],refkeys[idx]) for idx,key in enumerate(forgkeys)])
+            forkstrs = ',' + ','.join(['foreign key (%s) references %s(%s)'%(key,reftabs[idx],refkeys[idx]) for idx,key in enumerate(forgkeys)])
         else:
             forkstrs = ''
         tabopts = ') Engine=InnoDB default charset=' + charset + ' collate=' + charset + '_bin'
-        sql += cdefs + ',' + primstr + forkstrs + tabopts
+        sql += cdefs + primstr + forkstrs + tabopts
         try:
             with self.connection.cursor() as cursor:
                 self.result = cursor.execute(sql)
@@ -77,7 +77,7 @@ class sqlconn(object):
         values = tuple(valuelist)
         formats = ', '.join(['%s']*len(values))
         sql = "INSERT INTO `" + table + "` (" + keystr + ") VALUES (" + formats + ")"
-        print sql
+        #print sql
         try:
             with self.connection.cursor() as cursor:
                 self.result = cursor.execute(sql, values)
@@ -89,12 +89,12 @@ class sqlconn(object):
         return True, self.result
 
     def dropTab(self, table):
-        if table is None or len(len)==0:
+        if table is None or len(table)==0:
             return False, 'invalid table name'
         sql = 'drop table ' + table
         try:
             with self.connection.cursor() as cursor:
-                self.result = cursor.execute(sql, values)
+                self.result = cursor.execute(sql)
             self.connection.commit()
         except pymysql.err.Warning as e:
             return True, e
@@ -102,14 +102,31 @@ class sqlconn(object):
             return False, e
         return True, self.result
 
+    def dropTabs(self, tabs):
+        for tab in tabs:
+            self.dropTab(tab)
+
 if __name__ == '__main__':
     conn = sqlconn('localhost',3307,'root','usbw')
     if conn.connection.open:
         print 'opened'
-        print conn.createTab('article',False,True,
-        ['id','title','link','content','keyword_id','category_id','datetime','source_id'],
-         ['bigint','varchar(100)','varchar(120)','text','int','int','date','int'],
-          ['auto_increment','not null','not null','not null','not null','not null','not null','not null'],
+        print conn.createTab('keyword',['id','value'],['bigint','varchar(100)'],['auto_increment','not null'],['id'])
+        print conn.createTab('category',['id','parent_id','value'],['int','int','varchar(100)'],['auto_increment','',''],['id'])
+        print conn.createTab('source',['id','category_id','value'],['int','int','varchar(100)'],['auto_increment','not null','not null'],
+        ['id'],['category_id'],['category'],['id'])
+        print conn.createTab('user',['id','name','email'],['bigint','varchar(100)','varchar(100)'],
+        ['auto_increment','not null','not null'],['id'])
+        print conn.createTab('attach',
+        ['id','file_name','file_size','file_path','origin_addr'],
+        ['bigint','varchar(120)','bigint','varchar(120)','varchar(120)'],
+        ['auto_increment','not null','not null','not null','not null'],['id'])
+        print conn.createTab('article',
+        ['id','tid','title','link','content','keyword_id','category_id','datetime','source_id'],
+         ['bigint','bigint','varchar(100)','varchar(120)','text','int','int','date','int'],
+          ['auto_increment','not null','not null','not null','not null','not null','not null','not null','not null'],
            ['id'], [], [], [], 'utf8mb4')
-        print conn.insert('article',['title','link','content','keyword_id','category_id','datetime','source_id']
-        ,['test','http://i.whut.edu.cn','test text content',1,1,'2018-08-28',1])
+        print conn.createTab('attrelate',['id','article_id','attach_id'],['bigint','bigint','bigint'],
+        ['not null','not null','not null'],['id'],['article_id','attach_id'],['article','attach'],['id','id'])
+        print conn.insert('article',['tid','title','link','content','keyword_id','category_id','datetime','source_id']
+        ,[1,'test','http://i.whut.edu.cn','test text content',1,1,'2018-08-28',1])
+        #conn.dropTabs(['keyword','source','category','attrelate','article','attach','user'])
